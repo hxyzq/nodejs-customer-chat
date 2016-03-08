41766b23895aaf6d95d7b65b551ee04b06ae2ff2
王成浩 <719696499@qq.com>
2016-03-09 02:33:00 +0800

    精简代码采用对象的形式存储user staff service表 客服界面加入客服信息和接入用户窗口 修复正在服务列表更新的bug

--- chatserver.js
+++ chatserver.js
@@ -1,8 +1,8 @@
 var io = require('socket.io')();
 
-var onlineUsers = []; //在线用户列表
-var onlineStaffs = []; //在线客服列表
-var services = []; //当前服务列表
+var onlineUsers = {}; //在线用户列表
+var onlineStaffs = {}; //在线客服列表
+var services = {}; //当前服务列表
 
 //每隔5秒为用户分配一次客服
 setInterval(server, 5000);
@@ -16,6 +16,12 @@ io.on('connection', function(socket) {
 
   socket.on('disconnect', removeUserOrStaff);
 
+  socket.on('chat server', chatServer);
+
+  socket.on('user message', emitToStaff);
+
+  socket.on('staff message', emitToUser);
+
 });
 
 exports.listen = function(server) {
@@ -25,119 +31,113 @@ exports.listen = function(server) {
 
 //各类方法
 function server() {
-
-  if (onlineUsers.length === 0 || onlineStaffs.length === 0)
-    return;
-
-  var userfind = false; //用户找到标志
-  var stafffind = false; //客服找到标志
+  var user;
+  var staff;
   //获取当前未被服务的用户
-  var i;
-  for (i = onlineUsers.length - 1; i >= 0; i--) {
-    if (onlineUsers[i].isServed === false) {
-      userfind = true;
+  for (userid in onlineUsers) {
+    if (onlineUsers[userid].isServed === false) {
+      user = onlineUsers[userid];
       break;
     }
   }
-
   //获取当前可用的客服
-  var j;
-  for (j = onlineStaffs.length - 1; j >= 0; j--) {
-    if (onlineStaffs[j].maxserver - onlineStaffs[j].currentserver != 0) {
-      stafffind = true;
+  for (staffid in onlineStaffs) {
+    if (onlineStaffs[staffid].maxserver - onlineStaffs[staffid].currentserver !==0) {
+      staff = onlineStaffs[staffid];
       break;
     }
   }
 
-  if (userfind === false || stafffind === false)
-    return;
+  if (user === undefined || staff === undefined) return;
 
-  onlineUsers[i].isServed = true;
-  onlineStaffs[j].currentserver ++;
+  onlineUsers[user.userid].isServed = true;
+  onlineStaffs[staff.staffid].currentserver ++;
 
-  var service = {
-    user: onlineUsers[i],
-    staff: onlineStaffs[j],
-  }
-  services.unshift(service);
+  services[user.userid] = {
+    userid: userid,
+    staffid: staffid
+  };
 
-  console.log(onlineUsers[i].username + '---->' + onlineStaffs[j].staffname + ' successed!!');
+  //更新接入用户
+  onlineStaffs[staff.staffid].socket.emit('add served user', {
+    userid: onlineUsers[user.userid].userid,
+    username: onlineUsers[user.userid].username
+  });
 
-  //更新index页面正在服务列表
-  var servicelist = [];
-  for (var i = services.length - 1; i >= 0; i--) {
-    servicelist.unshift(services[i].user.username + '---->' + services[i].staff.staffname);
-  }
-  io.sockets.emit('update servicelist', servicelist);
+  console.log(user.username + '---->' + staff.staffname + ' successed!!');
+
+  //更新正在服务列表
+  updateServiceList();
+  //更新在线用户列表
+  updateUserList();
+  //更新在线客服列表
+  updateStaffList();
+
+}
+
+//客服聊天服务
+function chatServer(data) {
+
+  var staffid = data.staffid;
+  var userid = data.userid;
+
+  //向客服发送开始服务log
+  this.emit('log', '开始服务： ' + onlineUsers[userid].username);
+  //向用户发送分配客服成功log
+  onlineUsers[userid].socket.emit('log', onlineStaffs[staffid].staffname + '正在为您服务');
 
 }
 
 function addUser(data) {
 
   //将用户名和id存入socket
-  this.userid = data.id;
-  this.username = data.name;
+  this.userid = data.userid;
+  this.username = data.username;
 
-  console.log(data.name + '登入！');
+  console.log(data.username + '登入！');
 
   //将用户加入在线用户列表
-  var user = {
-    userid: data.id,
-    username: data.name,
-    isServed: false, //是否被服务
+  onlineUsers[data.userid] = {
+    userid: data.userid,
+    username: data.username,
+    isServed: false,
     socket: this
   };
-  onlineUsers.unshift(user);
 
-  this.emit('log', '连接成功！' + data.name + ' 欢迎登陆！');
+  this.emit('log', '连接成功！' + data.username + ' 欢迎登陆！');
 
-  //更新index页面在线用户列表
-  var userlist = [];
-  for (var i = onlineUsers.length - 1; i >= 0; i--) {
-    userlist.unshift(onlineUsers[i].username);
-  }
-  this.broadcast.emit('update userlist', userlist);
-
+  //更新在线用户列表
+  updateUserList();
 }
 
 function addStaff(data) {
 
   //将客服名和id存入socket
-  this.staffid = data.id;
-  this.staffname = data.name;
+  this.staffid = data.staffid;
+  this.staffname = data.staffname;
 
-  console.log(data.name + '登入！');
+  console.log(data.staffname + '登入！');
 
-  //将客服加入在线客服列表
-  var staff = {
-    staffid: data.id,
-    staffname: data.name,
-    maxserver: 2, //最多服务人数
-    currentserver: 0, //当前服务人数
+  onlineStaffs[data.staffid] = {
+    staffid: data.staffid,
+    staffname: data.staffname,
+    maxserver: 2,
+    currentserver: 0,
     socket: this
   };
-  onlineStaffs.unshift(staff);
 
-  //更新index页面在线用户列表
-  var userlist = [];
-  for (var i = onlineUsers.length - 1; i >= 0; i--) {
-    userlist.unshift(onlineUsers[i].username);
-  }
-  io.sockets.emit('update userlist', userlist);
+  //更新在线用户列表
+  updateUserList();
+  //更新在线客服列表
+  updateStaffList();
+  //更新正在服务列表
+  updateServiceList();
 
-  //更新index页面在线客服列表
-  var stafflist = [];
-  for (var i = onlineStaffs.length - 1; i >= 0; i--) {
-    stafflist.unshift(onlineStaffs[i].staffname);
-  }
-  io.sockets.emit('update stafflist', stafflist);
-
-  //更新index页面正在服务列表
-  var servicelist = [];
-  for (var i = services.length - 1; i >= 0; i--) {
-    servicelist.unshift(services[i].user.username + '---->' + services[i].staff.staffname);
-  }
-  io.sockets.emit('update servicelist', servicelist);
+  //更新index页面客服信息
+  this.emit('update staffinfo',{
+    staffid: data.staffid,
+    staffname: data.staffname
+  });
 
 }
 
@@ -147,68 +147,50 @@ function removeUserOrStaff(data) {
 
     console.log(this.username + '登出！');
     //更新onlineUsers列表
-    var user;
-    for (var i = onlineUsers.length - 1; i >= 0; i--) {
-      if (onlineUsers[i].userid == this.userid) {
-        user = onlineUsers[i];
-        onlineUsers.splice(i, 1);
-      }
-    }
-    this.broadcast.emit('remove user', {
-      name: this.username
+    var user = onlineUsers[this.userid];
+    delete onlineUsers[this.userid];
+    updateUserList();
+
+    //移除接入用户
+    onlineStaffs[services[user.userid].staffid].socket.emit('remove served user', {
+      userid: user.userid,
+      username: user.username
     });
+
     if (user.isServed === false) return; //如果该用户没有被服务，直接退出
 
-    //更新services列表
-    var service;
-    var tmp;
-    for (var i = services.length - 1; i >= 0; i--) {
-      if (services[i].user.userid === user.userid) {
-        tmp = services[i];
-        services.splice(i, 1);
-        service = tmp.user.username + '---->' + tmp.staff.staffname;
-      }
-    }
-    io.sockets.emit('remove service', {
-      service: service
-    });
-
     //对应客服服务数-1
-    for (var i = onlineStaffs.length - 1; i >= 0; i--) {
-      if (onlineStaffs[i].staffid === tmp.staff.staffid) {
-        onlineStaffs[i].currentserver --;
-      }
-    }
+    onlineStaffs[services[user.userid].staffid].currentserver --;
+    updateStaffList();
+    //更新services列表
+    delete services[user.userid];
+    updateServiceList();
 
-  } else {
+  } else if (this.staffname) {
 
     console.log(this.staffname + '登出！');
 
-    //更新onlineStaffs列表
-    var staff;
-    for (var i = onlineStaffs.length - 1; i >= 0; i--) {
-      if (onlineStaffs[i].staffid === this.staffid) {
-        staff = onlineStaffs[i];
-        onlineStaffs.splice(i, 1);
+    var staff = onlineStaffs[this.staffid];
+    // if (staff.currentserver === 0) return; //如果该客服没有正在服务直接退出
+
+    //更新用户的被服务状态
+    for (userid in services) {
+      if (services[userid].staffid == staff.staffid) {
+        onlineUsers[services[userid].userid].isServed = false;
+        updateUserList();
       }
     }
-    io.sockets.emit('remove staff', {
-      name: this.staffname
-    });
-
     //更新services列表
-    for (var i = services.length - 1; i >= 0; i--) {
-
-      if (services[i].staff.staffid === staff.staffid){
-        var service = services[i].user.username + '---->' + services[i].staff.staffname;
-        updateUserServed(services[i].user.userid); //更新用户的被服务状态
-        services.splice(i, 1);
-        io.sockets.emit('remove service', {
-          service: service
-        });
+    for (userid in services) {
+      if (services[userid].staffid == staff.staffid) {
+        delete services[userid];
+        updateServiceList();
       }
-
     }
+
+    //更新onlineStaffs列表(这一步必须最后进行)
+    delete onlineStaffs[staff.staffid];
+    updateStaffList();
 
   }
 
@@ -220,4 +202,56 @@ function updateUserServed(userid) {
         onlineUsers[i].isServed = false;
     }
   }
+}
+
+function updateUserList(data) {
+  var userlist = [];
+  for (userid in onlineUsers) {
+    userlist.unshift(onlineUsers[userid].username + '----' + onlineUsers[userid].isServed);
+  }
+  // userlist.reverse();
+  io.sockets.emit('update userlist', userlist);
+}
+
+function updateStaffList(data) {
+  var stafflist = [];
+  for (staffid in onlineStaffs) {
+    stafflist.unshift(onlineStaffs[staffid].staffname + '----' + onlineStaffs[staffid].currentserver);
+  }
+  // stafflist.reverse();
+  io.sockets.emit('update stafflist', stafflist);
+}
+
+function updateServiceList(data) {
+  var servicelist = [];
+  for (userid in services) {
+    // var userid = services[userid].userid;
+    // var staffid = services[userid].staffid;
+    servicelist.unshift(onlineUsers[services[userid].userid].username + '---->' + onlineStaffs[services[userid].staffid].staffname);
+  }
+  // servicelist.reverse();
+  io.sockets.emit('update servicelist', servicelist);
+}
+
+
+function emitToStaff(data) {
+  console.log(data.username + ': ' + data.message);
+
+  //获取用户和客服
+  var i;
+  for (i = services.length - 1; i >= 0; i--) {
+    if (services[i].user.userid == data.userid) {
+      break;
+    }
+  }
+  services[i].staff.socket.emit('new message', {
+    userid: services[i].user.userid,
+    username: services[i].user.username,
+    message: data.message
+  });
+
+}
+
+function emitToUser(data) {
+
 }
--- public/javascripts/user-chat-socket.js
+++ public/javascripts/user-chat-socket.js
@@ -14,18 +14,65 @@ $(function() {
 
       //用户登陆请求，传递用户名和id
       socket.emit('user login', {
-        id: userid,
-        name: username,
+        userid: userid,
+        username: username,
       });
 
   });
 
+  //收到服务器发来的 log
   socket.on('log', function(msg) {
     log(msg);
   });
 
+  //inputMessage检测回车发送消息
+  $('#inputMessage').keydown(function(_event) {
+   if(13 == _event.keyCode) {
+      sendMessage();
+      return false;
+   }
+  });
+
 
   //各类方法
+  function sendMessage() {
+    var message = $("#inputMessage").val();
+
+    message = cleanInput(message);
+
+    if (!message) { //如果过滤后的内容为空
+      return;
+    }
+
+    socket.emit('user message', {
+      userid: userid,
+      username: username,
+      message: message
+    });
+
+    // 有输入内容, 且已经连接成功, 且当前有客服和他匹配
+    // if (connected && linked) {
+    //   $inputMessage.val('');
+
+    //   addChatMessage({
+    //     color: '#008040',
+    //     username: username,
+    //     message: message
+    //   });
+
+    //   socket.emit('new message', {
+    //     id: userid,
+    //     message: message
+    //   });
+    // } else {
+    //   log(connected ? ( //连接成功, 但还没有 匹配成功
+    //     isKF ? '还没有访客接入, 不能发送消息 ...' : '客服忙, 请稍等 ...'
+    //   ) : '正在连接服务器...'); //未连接成功时的提示文字
+    // }
+  }
+  function cleanInput(text) {
+    return $('<div/>').text(text).text();
+  }
   function log(message) {
     addMessageElement($('<li>').addClass('log').text(message));
   }

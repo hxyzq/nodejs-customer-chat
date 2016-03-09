8545bed4ef88d7d7c30a8c3297bb78695909754e
王成浩 <719696499@qq.com>
2016-03-10 02:06:48 +0800

    将staff.html文件的js代码放入staff-chat-socket.js文件中调用

---
+++ public/javascripts/staff-chat-socket.js
@@ -0,0 +1,113 @@
+$(function() {
+
+  var socket = io();
+  // // var socket = io().connect('http://localhost:3000/', {
+  // //   'reconnect': true,
+  // //   'auto connect': true,
+  // //   'force new connection': true
+  // // });
+
+  var staffid = getParam('staffid');
+  var userid = getParam('userid');
+
+  log('正在连接服务器...');
+  socket.on('connect', function() {
+
+      //客服聊天请求，传递staffid和userid
+      socket.emit('chat server', {
+        staffid: staffid,
+        userid: userid,
+      });
+
+  });
+
+  //收到服务器发来的log
+  socket.on('log', function(msg) {
+    log(msg);
+  });
+
+  //inputMessage检测回车发送消息
+  $('#inputMessage').keydown(function(_event) {
+    if(13 == _event.keyCode) {
+      sendMessage();
+      return false;
+    }
+  });
+
+  socket.on('new message', function(data) {
+    addChatMessage({
+      username: data.username,
+      message: data.message
+    });
+  });
+
+  //各类方法
+  function sendMessage() {
+    var message = $("#inputMessage").val();
+
+    if (!message) { //如果内容为空
+      return;
+    }
+    $("#inputMessage").val('');
+
+    addChatMessage({
+      color: '#008040',
+      username: '客服' + staffid,
+      message: message
+    });
+
+    socket.emit('staff message', {
+      staffid: staffid,
+      staffname: '客服' + staffid,
+      userid: userid,
+      message: message
+    });
+
+  }
+  // Log a message
+  function log(message) {
+    addMessageElement($('<li>').addClass('log').text(message));
+  }
+  // Adds the visual chat message to the message list
+  function addChatMessage (data) {
+    var $usernameDiv = $('<span class="username"/>').text(data.username).css('color', data.color || '#00f');
+    var $messageBodyDiv = $('<span class="messageBody">').text(data.message);
+    var $messageDiv = $('<li class="message"/>')
+    .data('username', data.username)
+    .append($usernameDiv, formatDate(new Date()) + '<br/>', $messageBodyDiv);
+
+    addMessageElement($messageDiv);
+  }
+
+  //时间格式化
+  function formatDate(t) {
+    var M = t.getMonth() + 1,
+    D = t.getDate(),
+    H = t.getHours(),
+    m = t.getMinutes(),
+    s = t.getSeconds();
+
+    return [M, '-', D, ' ', H, ':', m, ':', s].join('');
+  }
+  function addMessageElement(el) {
+    $('#messages').append($(el).hide().fadeIn(200));
+    $('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
+  }
+
+  /*
+   * 获取指定的URL参数值
+   * URL:http://www.quwan.com/index?name=tyler
+   * 参数：paramName URL参数
+   * 调用方法:getParam("name")
+   * 返回值:tyler
+   */
+   function getParam(paramName) {
+    paramValue = "", isFound = !1;
+    if (this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
+      arrSource = unescape(this.location.search).substring(1, this.location.search.length).split("&"), i = 0;
+      while (i < arrSource.length && !isFound) arrSource[i].indexOf("=") > 0 && arrSource[i].split("=")[0].toLowerCase() == paramName.toLowerCase() && (paramValue = arrSource[i].split("=")[1], isFound = !0), i++
+    }
+    return paramValue == "" && (paramValue = null), paramValue
+  }
+
+});
